41ce8099ffba2b82e789f1a83d5bc8ebcd949e39
Dan Audne <dan.audne@gmail.com>
2013-01-13 03:19:11 +0100

    Made the animation for walking, running and pushing left and right at the same time

---
+++ main.js
@@ -0,0 +1,89 @@
+var animations = new Array();
+	animations[0] = [28,0,16,24]; //idle start
+	animations[1] = [0,30,16,24]; //idle left and right pressed
+	animations[2] = [199,0,16,24]; //idle left
+	animations[3] = [235,0,16,24]; //idle right
+	animations[4] = [169,0,16,24, 199,0,16,24]; //walking left
+	animations[5] = [235,0,16,24, 264,0,16,24]; //walking left
+	animations[6] = [84,0,16,24, 112,0,16,24]; //walking left
+	animations[7] = [321,0,16,24, 348,0,16,24]; //walking left
+var currentAnimation = 0;
+var frame = 0;
+var inAnimation = 0;
+var position = 0;
+var keyPressed = {};
+var oldKeyPressed = {};
+function init()
+{
+	document.addEventListener('keydown', function(e) {
+		keyPressed[e.keyCode] = true;
+	}, false);
+	document.addEventListener('keyup', function(e) {
+		keyPressed[e.keyCode] = false;
+		alert();
+	}, false);
+	var winHeight = $('body').height();
+	$('#bg').css({"padding-top": ((winHeight/2)-360)+"px"})
+
+	return setInterval(draw ,16);
+}
+
+function draw()
+{
+	if(inAnimation >= (animations[currentAnimation].length))
+	{
+		inAnimation = 0;
+	}
+	$('#luigi').css({'background-position-x': "-"+animations[currentAnimation][inAnimation]*2+"px", 'background-position-y': "-"+animations[currentAnimation][inAnimation+1]*2+"px"});
+	//$('#luigi').html(animations[currentAnimation][inAnimation]);
+	if((frame % 10) == 0)
+		inAnimation+=4;
+
+	checkKeys();
+
+//	$('#frames').html(frame);
+	frame++;
+}
+function checkKeys()
+{
+	if(keyPressed["37"] && keyPressed["39"]) {
+		currentAnimation = 1;
+	}
+	else if(keyPressed["37"])
+	{
+		if(keyPressed["90"])
+		{
+			currentAnimation = 6;
+			$('#luigi').css("left", "-=4");	
+		}else {
+			currentAnimation = 4;
+			$('#luigi').css("left", "-=2");	
+		}
+	}
+	else if(keyPressed["39"])
+	{
+		if(keyPressed["90"])
+		{
+			currentAnimation = 7;
+			$('#luigi').css("left", "+=4");
+		}else {
+			currentAnimation = 5;
+			$('#luigi').css("left", "+=2");
+		}
+	}
+	else if(keyPressed["38"])
+	{
+
+	}
+	else if(keyPressed["40"])
+	{
+		
+	}
+	else
+	{
+		if(currentAnimation == 4 || currentAnimation == 6)
+			currentAnimation = 2;
+		else if(currentAnimation == 5 || currentAnimation == 7)
+			currentAnimation = 3;
+	}
+}
